d0f16f28d2b1283cf04497b26a7d321e605a3ec8
R3RO <paulocesarbritocamilo@gmail.com>
2023-07-31 11:21:03 -0300

    add+

---
+++ index.js
@@ -0,0 +1,43 @@
+const ACORDION = [...window.document.querySelectorAll(".duvida > h2")]
+const duvidas_ocultadas = [...window.document.querySelectorAll(".duvida-ocultada")]
+const setas_acordion = [...window.document.querySelectorAll(".acordion")]
+
+
+
+
+
+
+ACORDION.map((e, i)=>{
+
+    e.addEventListener("click", ()=>{
+        
+        if (i===0){
+            duvidas_ocultadas[0].classList.toggle("duvida-ocultada-toggle")
+
+        } else if (i===1){
+            duvidas_ocultadas[1].classList.toggle("duvida-ocultada-toggle")
+        } else if (i===2){
+            duvidas_ocultadas[2].classList.toggle("duvida-ocultada-toggle")
+        } else if (i==3){
+            duvidas_ocultadas[3].classList.toggle("duvida-ocultada-toggle")
+        } else if (i===4){
+            duvidas_ocultadas[4].classList.toggle("duvida-ocultada-toggle")
+        } else if (i===5){
+            duvidas_ocultadas[5].classList.toggle("duvida-ocultada-toggle")
+        } else if (i===6){
+            duvidas_ocultadas[6].classList.toggle("duvida-ocultada-toggle")
+        } else if (i===7){
+            duvidas_ocultadas[7].classList.toggle("duvida-ocultada-toggle")
+        } else{
+            duvidas_ocultadas[8].classList.toggle("duvida-ocultada-toggle")
+        }
+ 
+
+    })
+
+
+
+})
+
+
+
