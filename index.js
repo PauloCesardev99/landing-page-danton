aa8351965dffb58995141cf16ed88b5213744d8f
R3RO <paulocesarbritocamilo@gmail.com>
2023-08-06 22:29:22 -0300

    fixed acorddion, email marketing add++

--- index.js
+++ index.js
@@ -12,24 +12,99 @@ ACORDION.map((e, i)=>{
     e.addEventListener("click", ()=>{
         
         if (i===0){
-            duvidas_ocultadas[0].classList.toggle("duvida-ocultada-toggle")
+            duvidas_ocultadas[0].style.display = "block"
+            duvidas_ocultadas[1].style.display = "none"
+            duvidas_ocultadas[2].style.display = "none"
+            duvidas_ocultadas[3].style.display = "none"
+            duvidas_ocultadas[4].style.display = "none"
+            duvidas_ocultadas[5].style.display = "none"
+            duvidas_ocultadas[6].style.display = "none"
+            duvidas_ocultadas[7].style.display = "none"
+            duvidas_ocultadas[8].style.display = "none"
 
         } else if (i===1){
-            duvidas_ocultadas[1].classList.toggle("duvida-ocultada-toggle")
+            duvidas_ocultadas[1].style.display = "block"
+            duvidas_ocultadas[0].style.display = "none"
+            duvidas_ocultadas[2].style.display = "none"
+            duvidas_ocultadas[3].style.display = "none"
+            duvidas_ocultadas[4].style.display = "none"
+            duvidas_ocultadas[5].style.display = "none"
+            duvidas_ocultadas[6].style.display = "none"
+            duvidas_ocultadas[7].style.display = "none"
+            duvidas_ocultadas[8].style.display = "none"
         } else if (i===2){
-            duvidas_ocultadas[2].classList.toggle("duvida-ocultada-toggle")
+            duvidas_ocultadas[2].style.display = "block"
+            duvidas_ocultadas[1].style.display = "none"
+            duvidas_ocultadas[0].style.display = "none"
+            duvidas_ocultadas[3].style.display = "none"
+            duvidas_ocultadas[4].style.display = "none"
+            duvidas_ocultadas[5].style.display = "none"
+            duvidas_ocultadas[6].style.display = "none"
+            duvidas_ocultadas[7].style.display = "none"
+            duvidas_ocultadas[8].style.display = "none"
+        
         } else if (i==3){
-            duvidas_ocultadas[3].classList.toggle("duvida-ocultada-toggle")
+            duvidas_ocultadas[3].style.display = "block"
+            duvidas_ocultadas[1].style.display = "none"
+            duvidas_ocultadas[0].style.display = "none"
+            duvidas_ocultadas[2].style.display = "none"
+            duvidas_ocultadas[4].style.display = "none"
+            duvidas_ocultadas[5].style.display = "none"
+            duvidas_ocultadas[6].style.display = "none"
+            duvidas_ocultadas[7].style.display = "none"
+            duvidas_ocultadas[8].style.display = "none"
         } else if (i===4){
-            duvidas_ocultadas[4].classList.toggle("duvida-ocultada-toggle")
+            duvidas_ocultadas[4].style.display = "block"
+            duvidas_ocultadas[1].style.display = "none"
+            duvidas_ocultadas[0].style.display = "none"
+            duvidas_ocultadas[2].style.display = "none"
+            duvidas_ocultadas[3].style.display = "none"
+            duvidas_ocultadas[5].style.display = "none"
+            duvidas_ocultadas[6].style.display = "none"
+            duvidas_ocultadas[7].style.display = "none"
+            duvidas_ocultadas[8].style.display = "none"
+          
         } else if (i===5){
-            duvidas_ocultadas[5].classList.toggle("duvida-ocultada-toggle")
+            duvidas_ocultadas[5].style.display = "block"
+            duvidas_ocultadas[1].style.display = "none"
+            duvidas_ocultadas[0].style.display = "none"
+            duvidas_ocultadas[2].style.display = "none"
+            duvidas_ocultadas[3].style.display = "none"
+            duvidas_ocultadas[4].style.display = "none"
+            duvidas_ocultadas[6].style.display = "none"
+            duvidas_ocultadas[7].style.display = "none"
+            duvidas_ocultadas[8].style.display = "none"
         } else if (i===6){
-            duvidas_ocultadas[6].classList.toggle("duvida-ocultada-toggle")
+            duvidas_ocultadas[6].style.display = "block"
+            duvidas_ocultadas[1].style.display = "none"
+            duvidas_ocultadas[0].style.display = "none"
+            duvidas_ocultadas[2].style.display = "none"
+            duvidas_ocultadas[3].style.display = "none"
+            duvidas_ocultadas[4].style.display = "none"
+            duvidas_ocultadas[5].style.display = "none"
+            duvidas_ocultadas[7].style.display = "none"
+            duvidas_ocultadas[8].style.display = "none"
+         
         } else if (i===7){
-            duvidas_ocultadas[7].classList.toggle("duvida-ocultada-toggle")
+            duvidas_ocultadas[7].style.display = "block"
+            duvidas_ocultadas[1].style.display = "none"
+            duvidas_ocultadas[0].style.display = "none"
+            duvidas_ocultadas[2].style.display = "none"
+            duvidas_ocultadas[3].style.display = "none"
+            duvidas_ocultadas[4].style.display = "none"
+            duvidas_ocultadas[5].style.display = "none"
+            duvidas_ocultadas[6].style.display = "none"
+            duvidas_ocultadas[8].style.display = "none"
         } else{
-            duvidas_ocultadas[8].classList.toggle("duvida-ocultada-toggle")
+            duvidas_ocultadas[8].style.display = "block"
+            duvidas_ocultadas[1].style.display = "none"
+            duvidas_ocultadas[0].style.display = "none"
+            duvidas_ocultadas[2].style.display = "none"
+            duvidas_ocultadas[3].style.display = "none"
+            duvidas_ocultadas[4].style.display = "none"
+            duvidas_ocultadas[5].style.display = "none"
+            duvidas_ocultadas[6].style.display = "none"
+            duvidas_ocultadas[7].style.display = "none"
         }
  
 
