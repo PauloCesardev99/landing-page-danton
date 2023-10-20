c8895121ca6b6eba941dd2966b823ffe728739e3
PauloCesardev99 <paulocesarbritocamilo@gmail.com>
2023-10-20 09:40:24 -0300

    fixed faq

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const ACORDION = [...window.document.querySelectorAll(".duvida > h2")]
+const ACORDION = [...window.document.querySelectorAll(".duvida > .duvida-flex > h2")]
 const duvidas_ocultadas = [...window.document.querySelectorAll(".duvida-ocultada")]
 
 
@@ -11,106 +11,108 @@ ACORDION.map((e, i)=>{
 
     e.addEventListener("click", ()=>{
         
-        if (i===0){
-            duvidas_ocultadas[0].style.display = "block"
-            duvidas_ocultadas[1].style.display = "none"
-            duvidas_ocultadas[2].style.display = "none"
-            duvidas_ocultadas[3].style.display = "none"
-            duvidas_ocultadas[4].style.display = "none"
-            duvidas_ocultadas[5].style.display = "none"
-            duvidas_ocultadas[6].style.display = "none"
-            duvidas_ocultadas[7].style.display = "none"
-            duvidas_ocultadas[8].style.display = "none"
-
-        } else if (i===1){
-            duvidas_ocultadas[1].style.display = "block"
-            duvidas_ocultadas[0].style.display = "none"
-            duvidas_ocultadas[2].style.display = "none"
-            duvidas_ocultadas[3].style.display = "none"
-            duvidas_ocultadas[4].style.display = "none"
-            duvidas_ocultadas[5].style.display = "none"
-            duvidas_ocultadas[6].style.display = "none"
-            duvidas_ocultadas[7].style.display = "none"
-            duvidas_ocultadas[8].style.display = "none"
-        } else if (i===2){
-            duvidas_ocultadas[2].style.display = "block"
-            duvidas_ocultadas[1].style.display = "none"
-            duvidas_ocultadas[0].style.display = "none"
-            duvidas_ocultadas[3].style.display = "none"
-            duvidas_ocultadas[4].style.display = "none"
-            duvidas_ocultadas[5].style.display = "none"
-            duvidas_ocultadas[6].style.display = "none"
-            duvidas_ocultadas[7].style.display = "none"
-            duvidas_ocultadas[8].style.display = "none"
         
-        } else if (i==3){
-            duvidas_ocultadas[3].style.display = "block"
-            duvidas_ocultadas[1].style.display = "none"
-            duvidas_ocultadas[0].style.display = "none"
-            duvidas_ocultadas[2].style.display = "none"
-            duvidas_ocultadas[4].style.display = "none"
-            duvidas_ocultadas[5].style.display = "none"
-            duvidas_ocultadas[6].style.display = "none"
-            duvidas_ocultadas[7].style.display = "none"
-            duvidas_ocultadas[8].style.display = "none"
-        } else if (i===4){
-            duvidas_ocultadas[4].style.display = "block"
-            duvidas_ocultadas[1].style.display = "none"
-            duvidas_ocultadas[0].style.display = "none"
-            duvidas_ocultadas[2].style.display = "none"
-            duvidas_ocultadas[3].style.display = "none"
-            duvidas_ocultadas[5].style.display = "none"
-            duvidas_ocultadas[6].style.display = "none"
-            duvidas_ocultadas[7].style.display = "none"
-            duvidas_ocultadas[8].style.display = "none"
+        if(i===0){
+            duvidas_ocultadas[0].classList.toggle("duvida-aparacer")
+            duvidas_ocultadas[1].classList.remove("duvida-aparacer")
+            duvidas_ocultadas[2].classList.remove("duvida-aparacer")
+            duvidas_ocultadas[3].classList.remove("duvida-aparacer")
+            duvidas_ocultadas[4].classList.remove("duvida-aparacer")
+            duvidas_ocultadas[5].classList.remove("duvida-aparacer")
+            duvidas_ocultadas[6].classList.remove("duvida-aparacer")
+            duvidas_ocultadas[7].classList.remove("duvida-aparacer")
+            duvidas_ocultadas[8].classList.remove("duvida-aparacer")
           
-        } else if (i===5){
-            duvidas_ocultadas[5].style.display = "block"
-            duvidas_ocultadas[1].style.display = "none"
-            duvidas_ocultadas[0].style.display = "none"
-            duvidas_ocultadas[2].style.display = "none"
-            duvidas_ocultadas[3].style.display = "none"
-            duvidas_ocultadas[4].style.display = "none"
-            duvidas_ocultadas[6].style.display = "none"
-            duvidas_ocultadas[7].style.display = "none"
-            duvidas_ocultadas[8].style.display = "none"
-        } else if (i===6){
-            duvidas_ocultadas[6].style.display = "block"
-            duvidas_ocultadas[1].style.display = "none"
-            duvidas_ocultadas[0].style.display = "none"
-            duvidas_ocultadas[2].style.display = "none"
-            duvidas_ocultadas[3].style.display = "none"
-            duvidas_ocultadas[4].style.display = "none"
-            duvidas_ocultadas[5].style.display = "none"
-            duvidas_ocultadas[7].style.display = "none"
-            duvidas_ocultadas[8].style.display = "none"
-         
-        } else if (i===7){
-            duvidas_ocultadas[7].style.display = "block"
-            duvidas_ocultadas[1].style.display = "none"
-            duvidas_ocultadas[0].style.display = "none"
-            duvidas_ocultadas[2].style.display = "none"
-            duvidas_ocultadas[3].style.display = "none"
-            duvidas_ocultadas[4].style.display = "none"
-            duvidas_ocultadas[5].style.display = "none"
-            duvidas_ocultadas[6].style.display = "none"
-            duvidas_ocultadas[8].style.display = "none"
+        } else if(i===1){
+            duvidas_ocultadas[1].classList.toggle("duvida-aparacer")
+            duvidas_ocultadas[0].classList.remove("duvida-aparacer")
+            duvidas_ocultadas[2].classList.remove("duvida-aparacer")
+            duvidas_ocultadas[3].classList.remove("duvida-aparacer")
+            duvidas_ocultadas[4].classList.remove("duvida-aparacer")
+            duvidas_ocultadas[5].classList.remove("duvida-aparacer")
+            duvidas_ocultadas[6].classList.remove("duvida-aparacer")
+            duvidas_ocultadas[7].classList.remove("duvida-aparacer")
+            duvidas_ocultadas[8].classList.remove("duvida-aparacer")
+           
+        } else if(i===2){
+            duvidas_ocultadas[2].classList.toggle("duvida-aparacer")
+            duvidas_ocultadas[1].classList.remove("duvida-aparacer")
+            duvidas_ocultadas[0].classList.remove("duvida-aparacer")
+            duvidas_ocultadas[3].classList.remove("duvida-aparacer")
+            duvidas_ocultadas[4].classList.remove("duvida-aparacer")
+            duvidas_ocultadas[5].classList.remove("duvida-aparacer")
+            duvidas_ocultadas[6].classList.remove("duvida-aparacer")
+            duvidas_ocultadas[7].classList.remove("duvida-aparacer")
+            duvidas_ocultadas[8].classList.remove("duvida-aparacer")
+
+
+        } else if(i===3){
+            duvidas_ocultadas[3].classList.toggle("duvida-aparacer")
+            duvidas_ocultadas[1].classList.remove("duvida-aparacer")
+            duvidas_ocultadas[2].classList.remove("duvida-aparacer")
+            duvidas_ocultadas[0].classList.remove("duvida-aparacer")
+            duvidas_ocultadas[4].classList.remove("duvida-aparacer")
+            duvidas_ocultadas[5].classList.remove("duvida-aparacer")
+            duvidas_ocultadas[6].classList.remove("duvida-aparacer")
+            duvidas_ocultadas[7].classList.remove("duvida-aparacer")
+            duvidas_ocultadas[8].classList.remove("duvida-aparacer")
+        } else if(i===4){
+            duvidas_ocultadas[4].classList.toggle("duvida-aparacer")
+            duvidas_ocultadas[1].classList.remove("duvida-aparacer")
+            duvidas_ocultadas[2].classList.remove("duvida-aparacer")
+            duvidas_ocultadas[3].classList.remove("duvida-aparacer")
+            duvidas_ocultadas[0].classList.remove("duvida-aparacer")
+            duvidas_ocultadas[5].classList.remove("duvida-aparacer")
+            duvidas_ocultadas[6].classList.remove("duvida-aparacer")
+            duvidas_ocultadas[7].classList.remove("duvida-aparacer")
+            duvidas_ocultadas[8].classList.remove("duvida-aparacer")
+        } else if(i===5){
+            duvidas_ocultadas[5].classList.toggle("duvida-aparacer")
+            duvidas_ocultadas[1].classList.remove("duvida-aparacer")
+            duvidas_ocultadas[2].classList.remove("duvida-aparacer")
+            duvidas_ocultadas[3].classList.remove("duvida-aparacer")
+            duvidas_ocultadas[4].classList.remove("duvida-aparacer")
+            duvidas_ocultadas[0].classList.remove("duvida-aparacer")
+            duvidas_ocultadas[6].classList.remove("duvida-aparacer")
+            duvidas_ocultadas[7].classList.remove("duvida-aparacer")
+            duvidas_ocultadas[8].classList.remove("duvida-aparacer")
+        } else if(i===6){
+            duvidas_ocultadas[6].classList.toggle("duvida-aparacer")
+            duvidas_ocultadas[1].classList.remove("duvida-aparacer")
+            duvidas_ocultadas[2].classList.remove("duvida-aparacer")
+            duvidas_ocultadas[3].classList.remove("duvida-aparacer")
+            duvidas_ocultadas[4].classList.remove("duvida-aparacer")
+            duvidas_ocultadas[5].classList.remove("duvida-aparacer")
+            duvidas_ocultadas[0].classList.remove("duvida-aparacer")
+            duvidas_ocultadas[7].classList.remove("duvida-aparacer")
+            duvidas_ocultadas[8].classList.remove("duvida-aparacer")
+        } else if(i===7){
+            duvidas_ocultadas[7].classList.toggle("duvida-aparacer")
+            duvidas_ocultadas[1].classList.remove("duvida-aparacer")
+            duvidas_ocultadas[2].classList.remove("duvida-aparacer")
+            duvidas_ocultadas[3].classList.remove("duvida-aparacer")
+            duvidas_ocultadas[4].classList.remove("duvida-aparacer")
+            duvidas_ocultadas[5].classList.remove("duvida-aparacer")
+            duvidas_ocultadas[6].classList.remove("duvida-aparacer")
+            duvidas_ocultadas[0].classList.remove("duvida-aparacer")
+            duvidas_ocultadas[8].classList.remove("duvida-aparacer")
         } else{
-            duvidas_ocultadas[8].style.display = "block"
-            duvidas_ocultadas[1].style.display = "none"
-            duvidas_ocultadas[0].style.display = "none"
-            duvidas_ocultadas[2].style.display = "none"
-            duvidas_ocultadas[3].style.display = "none"
-            duvidas_ocultadas[4].style.display = "none"
-            duvidas_ocultadas[5].style.display = "none"
-            duvidas_ocultadas[6].style.display = "none"
-            duvidas_ocultadas[7].style.display = "none"
+            duvidas_ocultadas[8].classList.toggle("duvida-aparacer")
+            duvidas_ocultadas[1].classList.remove("duvida-aparacer")
+            duvidas_ocultadas[2].classList.remove("duvida-aparacer")
+            duvidas_ocultadas[3].classList.remove("duvida-aparacer")
+            duvidas_ocultadas[4].classList.remove("duvida-aparacer")
+            duvidas_ocultadas[5].classList.remove("duvida-aparacer")
+            duvidas_ocultadas[6].classList.remove("duvida-aparacer")
+            duvidas_ocultadas[7].classList.remove("duvida-aparacer")
+            duvidas_ocultadas[0].classList.remove("duvida-aparacer")
         }
- 
 
-    })
+      
 
 
+
+})
 
 })
 
